790d7b1f0cd391d8bba4eaac04109b74a75b6371
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-01-19 23:48:51 +0100

    home page basic layout

--- src/components/AppBody/AppBody.js
+++ src/components/AppBody/AppBody.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './_appBody.scss'
+import HomePage from "../HomePage/HomePage";
+import NewFlashcard from "../NewFlashcard/NewFlashcard";
+
 
 export default function AppBody() {
     return (
         <main className="mainApp">
             <div className="container">
-
+                <HomePage />
+                <NewFlashcard />
             </div>
         </main>
     )
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './_homePage.scss';
+
+export default function HomePage () {
+    return (
+        <div className="homepage">
+            <div className="optionBox">
+                <p className="option__yourFlashcards">
+                    Your<br/>flashcards
+                </p>
+            </div>
+            <div className="optionBox">
+                <p className="option__newFlashcard">
+                    Add<br/>flashcard
+                </p>
+            </div>
+        </div>
+    )
+}
