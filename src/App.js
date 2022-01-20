c65cbe44b6251bbd5997ca50a83569bf0915fe2c
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-01-20 23:39:28 +0100

    conection to firebase done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import './App.scss';
 import './basic_scss/_variables.scss'
 import Header from "./components/header/Header";
 import AppBody from "./components/AppBody/AppBody";
-import data from "./firebaseConfig";
-
-console.log(data)
 
 function App() {
   return (
--- src/components/AppBody/AppBody.js
+++ src/components/AppBody/AppBody.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './_appBody.scss'
 import HomePage from "../HomePage/HomePage";
 import NewFlashcard from "../NewFlashcard/NewFlashcard";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import colRef from "../../firebaseConfig";
+import {getDocs} from "firebase/firestore";
+
 
 
 export default function AppBody() {
+    const [files, setFiles] = useState([]);
+    console.log(files);
+
+    useEffect(() => {
+        getDocs(colRef).then((snapshot) => {
+            let flashcards = [];
+            snapshot.docs.forEach((doc) => {
+                flashcards.push({...doc.data(), id: doc.id})
+            })
+            setFiles([...flashcards])
+        })
+    }, [])
+
     return (
         <main className="mainApp">
             <div className="container">
