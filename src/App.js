2bbceda53b47274539af18b50f1243e1ae5cba65
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-01-20 18:43:45 +0100

    firebase configuration - not ended

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.scss';
 import './basic_scss/_variables.scss'
 import Header from "./components/header/Header";
 import AppBody from "./components/AppBody/AppBody";
+import data from "./firebaseConfig";
+
+console.log(data)
 
 function App() {
   return (
--- src/components/NewFlashcard/NewFlashcard.js
+++ src/components/NewFlashcard/NewFlashcard.js
@@ -1,37 +1,82 @@
-import React from "react";
+import React, {useState} from "react";
 import './_newFlashcard.scss'
 import {Link} from "react-router-dom";
 
 export default function NewFlashcard() {
+    const [field, setField] = useState("");
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
+    const [filters, setFilters] = useState([]);
+    const [filter, setFilter] = useState("");
+
+    const handleAddFilters = (filter) => {
+        setFilters([...filters, filter])
+        setFilter("")
+    }
+
+    const handleAddFlashcard = (e) => {
+        e.preventDefault()
+
+        const newFlashcard = {
+            field,
+            question,
+            answer,
+            filters,
+            status: "learning"
+        }
+    }
+
+
     return (
         <form onSubmit={(e) => e.preventDefault() }>
             <section className="flashcardInfo">
                 <div className="flashcardInfo__basic">
                     <div className="field">
                         <label htmlFor="field">Field</label>
-                        <input type="text" id="field"/>
+                        <input type="text"
+                               id="field"
+                               value={field}
+                               onChange={e => setField(e.target.value)}
+                        />
                     </div>
                     <div className="question">
                         <label htmlFor="question">Question</label>
-                        <textarea name="question" id="question" rows="5"/>
+                        <textarea name="question"
+                                  id="question"
+                                  rows="5"
+                                  value={question}
+                                  onChange={e => setQuestion(e.target.value)}
+                        />
                     </div>
                     <div className="answer">
                         <label htmlFor="answer">Answer</label>
-                        <textarea name="answer" id="answer" rows="5"/>
+                        <textarea name="answer"
+                                  id="answer"
+                                  rows="5"
+                                  value={answer}
+                                  onChange={e => setAnswer(e.target.value)}
+                        />
                     </div>
                 </div>
                 <div className="flashcardInfo__filters">
                     <label htmlFor="filter">Your filters</label>
                     <div className="filters__adding">
-                        <input type="text" id="filter"/>
-                        <button className="addButton">
+                        <input type="text"
+                               id="filter"
+                               value={filter}
+                               onChange={e => setFilter(e.target.value)}
+                        />
+                        <button className="addButton" onClick={() => handleAddFilters(filter)}>
                             <i> </i>
                         </button>
                     </div>
+                    <ul>
+                        {filters.map(filter => <li key={filter}>{filter}</li>)}
+                    </ul>
                 </div>
             </section>
             <div className="buttonBox">
-                <Link className="btn btn__submit" to="/">Add</Link>
+                <Link className="btn btn__submit" to="/" onClick={handleAddFlashcard}>Add</Link>
                 <Link className="btn btn__cancel" to="/">Cancel</Link>
             </div>
         </form>
